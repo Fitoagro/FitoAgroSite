31d92553a9e8af49147b7186d8dc2e3dfafdd632
MiguelASCorreia <mas.correia@campus.fct.unl.pt>
2021-05-03 14:39:57 +0100

    Notas no mapa e nos dados raw

--- rawdataprocess.js
+++ rawdataprocess.js
@@ -121,6 +121,98 @@ function generateTrapTable(table, map){
     });
     return nonZeros;
 }
+
+function fillNotes(genericNote, specificNotes){
+    var note_table = document.getElementById("note_table");
+
+    if(isUndefined(genericNote) && isUndefined(specificNotes)){
+        note_table.style.display = 'none';
+        document.getElementById('no_notes').style.display = 'block';
+        return;
+    }
+
+
+    let row = note_table.insertRow();
+    if(!isUndefined(genericNote)){
+        let type = document.createTextNode("Genérica");
+        let timestamp = document.createTextNode("- - -");
+        let valueText = document.createTextNode(genericNote.value);
+
+        let cell = row.insertCell();
+        cell.style.textAlign = 'center';
+        cell.appendChild(type);
+        cell = row.insertCell();
+        cell.style.textAlign = 'center';
+        cell.appendChild(timestamp);cell = row.insertCell();
+        cell.style.textAlign = 'center';
+        cell.appendChild(valueText);
+    }
+
+    if(!isUndefined(specificNotes)) {
+        Object.keys(specificNotes).sort().forEach(function (noteIndex) {
+            let row = note_table.insertRow();
+            let cell;
+            let observation = specificNotes[noteIndex]['observation'];
+            observation = getObservationText(observation);
+            if (!isUndefined(observation)) {
+                let observationText = document.createTextNode(observation);
+                cell = row.insertCell();
+                cell.style.textAlign = 'center';
+                cell.appendChild(observationText);
+            }else{
+                let empty = document.createTextNode("- - -");
+                cell = row.insertCell();
+                cell.style.textAlign = 'center';
+                cell.appendChild(empty);
+            }
+
+            let timestamp = specificNotes[noteIndex]['creation_timestamp']; //20210426_092643
+            let timestampText = document.createTextNode(timestamp.substr(9, 2) + ":" + timestamp.substr(11, 2));
+
+            cell = row.insertCell();
+            cell.style.textAlign = 'center';
+            cell.appendChild(timestampText);
+
+            let value = specificNotes[noteIndex]['content'];
+            let valueText = document.createTextNode(value);
+
+            cell = row.insertCell();
+            cell.appendChild(valueText);
+        });
+    }
+
+    generateTableHead(note_table, ["Observação","Hora","Nota"]);
+}
+
+function getObservationText(observation){
+    console.log(observation);
+    if(observation != null) {
+        let processedObservation = null;
+        try {
+            let infoJSON = JSON.parse(observation);
+            if (!isUndefined(infoJSON["eoi"])) {
+                let message = "Árvore " + (parseInt(infoJSON["eoi"]) + 1) + " " + infoJSON["protocol_name"] + ": " + infoJSON["observation_name"];
+                if(observation.includes("title"))
+                    message+= " - " + infoJSON["title"];
+                processedObservation = message;
+            }else if(!isUndefined(infoJSON["title"])){
+                processedObservation ="Armadilha: " + infoJSON["title"];
+            }else if(!isUndefined(infoJSON["occurrence_type"])){
+                let occ_type = parseInt(infoJSON["occurrence_type"]) === 0 ? "Ação: " : "Evento: ";
+                processedObservation = occ_type + infoJSON["occurrence_name"];
+
+            }
+            return processedObservation;
+
+        } catch (e) {
+            console.log(e);
+
+            return undefined;
+        }
+
+    }
+    return undefined
+}
 function getProcessedEois(rawJson){
     var maxEoiCount = 0;
     var map = {};
@@ -130,7 +222,7 @@ function getProcessedEois(rawJson){
     var rawDate = rawJson["time_begin"].split('.')[0];
     var defaultObservations = rawJson['info']['Default_observations'];
     var caliber = defaultObservations['Calibre'];
-    var notes = defaultObservations['Notas'];
+    var genericNote = defaultObservations['Notas'];
 
     document.getElementById("date").innerText = rawDate.substring(0,4) + "/" + rawDate.substring(4,6) + "/" + rawDate.substring(6,8);
     document.getElementById("pob").innerText = rawJson['plot_name'];
@@ -139,82 +231,95 @@ function getProcessedEois(rawJson){
     document.getElementById("rain").innerText = defaultObservations['Chuva'].value  === "true" ? "Sim" : "Não";
     document.getElementById("treatment").innerText = defaultObservations['Tratamento'].value  === "true" ? "Sim" : "Não";
     document.getElementById("caliber").innerText = typeof  caliber === 'undefined' ? 'Não registado' : caliber.value;
-    document.getElementById("notes").innerText = typeof  notes === 'undefined' ? 'Não registado' : notes.value;
+    //document.getElementById("notes").innerText = typeof  notes === 'undefined' ? 'Não registado' : notes.value;
+
+    fillNotes(genericNote,rawJson['notes']);
+
+    var eoi_table = document.getElementById("eoi_table");
 
     var eoiJson = rawJson['eois'];
-    Object.keys(eoiJson).forEach(function(eoi) {
+    if(!isUndefined(eoiJson)){
+        Object.keys(eoiJson).forEach(function(eoi) {
 
-        var eoiInt = parseInt(eoi);
+            var eoiInt = parseInt(eoi);
 
-        var protocolsByEoi = eoiJson[eoiInt];
-        if(maxEoiCount < eoiInt)
-            maxEoiCount = eoiInt;
-        Object.keys(protocolsByEoi).forEach(function(protocol) {
-            var obsPerProtocol = protocolsByEoi[protocol];
+            var protocolsByEoi = eoiJson[eoiInt];
+            if(maxEoiCount < eoiInt)
+                maxEoiCount = eoiInt;
+            Object.keys(protocolsByEoi).forEach(function(protocol) {
+                var obsPerProtocol = protocolsByEoi[protocol];
 
-            if(typeof flagPerProtocol[protocol] === "undefined") {
-                flagPerProtocol[protocol] = false;
-            }
+                if(typeof flagPerProtocol[protocol] === "undefined") {
+                    flagPerProtocol[protocol] = false;
+                }
 
-            Object.keys(obsPerProtocol).forEach(function(obs) {
-                var componentPerObservation = obsPerProtocol[obs];
-                Object.keys(componentPerObservation).forEach(function(component) {
-                    var componentData = componentPerObservation[component];
+                Object.keys(obsPerProtocol).forEach(function(obs) {
+                    var componentPerObservation = obsPerProtocol[obs];
+                    Object.keys(componentPerObservation).forEach(function(component) {
+                        var componentData = componentPerObservation[component];
 
-                    var description = componentData['description'];
-                    var rawValue = componentData['value'];
+                        var description = componentData['description'];
+                        var rawValue = componentData['value'];
 
-                    if(rawValue === "true") rawValue = "1";
-                    else if(rawValue === "false") rawValue = "0";
+                        if(rawValue === "true") rawValue = "1";
+                        else if(rawValue === "false") rawValue = "0";
 
-                    if(rawValue !== "0" && flagPerProtocol[protocol] === false) {
-                        flagPerProtocol[protocol] = true;
-                    }
+                        if(rawValue !== "0" && flagPerProtocol[protocol] === false) {
+                            flagPerProtocol[protocol] = true;
+                        }
 
 
-                    if(isUndefined(map[protocol]))
-                        map[protocol] = {};
-                    if(isUndefined(map[protocol][obs]))
-                        map[protocol][obs] = {};
-                    if(isUndefined(map[protocol][obs][description]))
-                        map[protocol][obs][description] = [];
+                        if(isUndefined(map[protocol]))
+                            map[protocol] = {};
+                        if(isUndefined(map[protocol][obs]))
+                            map[protocol][obs] = {};
+                        if(isUndefined(map[protocol][obs][description]))
+                            map[protocol][obs][description] = [];
 
-                    map[protocol][obs][description][eoiInt] = rawValue;
+                        map[protocol][obs][description][eoiInt] = rawValue;
 
+
+                    });
 
                 });
 
             });
 
         });
+        Object.keys(flagPerProtocol).forEach(function(protocol) {
+            if(flagPerProtocol[protocol] === false)
+                delete map[protocol];
+        });
 
-    });
-    Object.keys(flagPerProtocol).forEach(function(protocol) {
-        if(flagPerProtocol[protocol] === false)
-            delete map[protocol];
-    });
-
-    var eoi_table = document.getElementById("eoi_table");
-
-    if(Object.keys(map).length !== 0){
-        const arr = Array.from({length: maxEoiCount + 1}, (_, index) => index + 1);
-        let data = ["Protocolo", "Observação","Árvore nº."];
-        for(var i = 0; i < arr.length; i++){
-            data.push(arr[i].toString());
+        if(Object.keys(map).length !== 0){
+            const arr = Array.from({length: maxEoiCount + 1}, (_, index) => index + 1);
+            let data = ["Protocolo", "Observação","Árvore nº."];
+            for(var i = 0; i < arr.length; i++){
+                data.push(arr[i].toString());
+            }
+            generateEOITable(eoi_table, map);
+            generateTableHead(eoi_table, data);
+        }else {
+            eoi_table.style.display = 'none';
+            document.getElementById('no_data_eoi').style.display = 'block';
         }
-        generateEOITable(eoi_table, map);
-        generateTableHead(eoi_table, data);
-    }else {
+    }else{
         eoi_table.style.display = 'none';
         document.getElementById('no_data_eoi').style.display = 'block';
     }
 
-    var trapJson = rawJson['traps'];
-    let trapKeys = Object.keys(trapJson).sort();
     var trap_table = document.getElementById("trap_table");
-    let nonZeros = generateTrapTable(trap_table, trapJson);
-    generateTableHead(trap_table, trapKeys);
-    if(nonZeros === 0){
+
+    var trapJson = rawJson['traps'];
+    if(!isUndefined(trapJson)) {
+        let trapKeys = Object.keys(trapJson).sort();
+        let nonZeros = generateTrapTable(trap_table, trapJson);
+        generateTableHead(trap_table, trapKeys);
+        if (nonZeros === 0) {
+            trap_table.style.display = 'none';
+            document.getElementById('no_data_trap').style.display = 'block';
+        }
+    }else{
         trap_table.style.display = 'none';
         document.getElementById('no_data_trap').style.display = 'block';
     }
