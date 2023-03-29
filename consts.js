049a3ec8bfee09173142c3ff6dbbe28597a0a1b0
MigueASCorreia <mas.correia@campus.fct.unl.pt>
2023-03-29 15:29:44 +0100

    classe com constantes

---
+++ consts.js
@@ -0,0 +1,18 @@
+export class GoogleConst {
+    static get API_KEY() { return 'AIzaSyDid5jJUjIBp7BCKZwR1LI0G87b4yEBAhY'; }
+    static get CLIENT_ID() { return '683847325816-66urtf71ciqlbrjp8dqks5v4nmoks9ih.apps.googleusercontent.com'; }
+    static get DISCOVERY_DOC() { return 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'; }
+    static get SCOPES() { return 'https://www.googleapis.com/auth/drive'; }
+}
+
+export class Map{
+    static get ACCESS_TOKEN(){return 'pk.eyJ1IjoibWlndWVsYXNjb3JyZWlhIiwiYSI6ImNrN25iYzh4NTAxMHIzc210bzJpM3EyZDcifQ.pVWzek_lEMfJixSnBu1ZMA'}
+}
+
+export class DriveIds{
+    static get UPLOAD_FOLDER() {return '1G_jn23JzqHhyGidtzV85rPGIMIOE6Pt-'}
+    static get REGION_INFO_FILE() {return '1ic6NxWicelaOBh6zvxYkdqSv7P4O5TF7'}
+    static get ENEMY_INFO_FILE() {return '1JPzl55H0zMviO5R8euyWqTLE9t94XJN1'}
+    static get PLOT_FILE() {return '1Uu9L5e2u3stdskQQpDcQwBdShxMtWmyk'}
+    static get COMMENTS_FOLDER() {return '1nD2l8AXjrqnP5B9bXJ-0YVzBwqOfEEfY'}
+}
